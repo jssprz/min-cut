09b07623a9f2b1d48b8062b7f22a1d25ee9d3ad5
Jesus Perez Martin <jperezmartin90@gmail.com>
2018-08-02 03:01:38 -0400

    Changes the main function. And defines variables for the min and max number of vertices to test

--- main.cpp
+++ main.cpp
@@ -2,3 +2,47 @@
 // Created by jeperez on 31-07-18.
 //
 
+#include "config.h"
+#include <numeric>
+#include <iostream>
+
+#ifdef USE_EXPERIMENTS
+#include "experiments/experiments.h"
+#endif
+
+#ifdef USE_DETERMINISTIC
+#endif
+
+#ifdef USE_PROBABILISTIC
+#endif
+
+#ifdef USE_HYBRID
+#endif
+
+#ifdef USE_EXPERIMENTS
+using namespace experiments;
+#endif
+
+void main(){
+
+#ifdef USE_EXPERIMENTS
+
+#ifdef USE_DETERMINISTIC
+#endif
+
+#ifdef USE_PROBABILISTIC
+#endif
+
+#ifdef USE_HYBRID
+#endif
+
+#endif
+
+    vector<int> V(100);
+    std::iota(std::begin(V), std::end(V), 0);
+
+    for (int i = 0; i < TRIALS; ++i) {
+        auto G = random_graph<int>(V, 0.5);
+        cout << "edges count: " << G.E.size() << endl;
+    }
+}
